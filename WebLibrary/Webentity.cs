1a0ebd7dd4c9fc121ffefaf3a9cf0cae63f120a6
santhosh <santhoshcellamuthuc@gmail.com>
2024-07-26 18:45:56 +0530

    create for entity

---
+++ WebLibrary/Webentity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebLibrary
+{
+    public class Webentity
+    {
+        [Key]
+        public string name { get; set; }
+        public DateTime brithdate { get; set; }
+        public long experience { get; set; }
+        public long exceptedsalary {get;set;}
+    }
+}
